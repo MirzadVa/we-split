796853c8240c3bd82226458c048b38c61360d521
Mirzad <52415551+MirzadVa@users.noreply.github.com>
2024-02-03 12:47:07 +0100

    Initial Commit

---
+++ we-split/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  we-split
+//
+//  Created by Mirzad Varupa on 3. 2. 2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ we-split/we_splitApp.swift
@@ -0,0 +1,17 @@
+//
+//  we_splitApp.swift
+//  we-split
+//
+//  Created by Mirzad Varupa on 3. 2. 2024..
+//
+
+import SwiftUI
+
+@main
+struct we_splitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
